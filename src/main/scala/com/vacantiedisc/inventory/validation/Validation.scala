2749e7b2236df4dd2ecd4a4573d8bdf80f3d589e
gregory <gregory@xite.com>
2019-12-24 18:21:42 +0100

    add lost files

---
+++ src/main/scala/com/vacantiedisc/inventory/validation/Validation.scala
@@ -0,0 +1,59 @@
+package com.vacantiedisc.inventory.validation
+
+import com.vacantiedisc.inventory.http.models.{
+  BookingOverviewRequest,
+  BookingRequest
+}
+import com.vacantiedisc.inventory.utils.DateUtils
+import org.joda.time.LocalDate
+
+/**
+  * Validate row entity
+  * for now it validates only credit limit and birthday
+  * possible to extend check postcode, phone number etc
+  */
+object Validation {
+
+  import cats.data.ValidatedNel
+  import cats.implicits._
+
+  trait Validator[T, U] {
+    def validate(entity: T): ValidatedNel[String, U]
+  }
+
+  object Validator {
+    def validate[T, U](
+      entity: T
+    )(implicit v: Validator[T, U]): ValidatedNel[String, U] =
+      v.validate(entity)
+  }
+
+  val WRONG_DATE_FORMAT = "Wrong Date time format"
+  val WRONG_AMOUNT_FORMAT = "Amount must be more than 0"
+
+  def validateAmount(
+    br: BookingRequest
+  ): ValidatedNel[String, BookingRequest] = {
+    Either.cond(br.amount > 0, br, WRONG_AMOUNT_FORMAT).toValidatedNel
+  }
+
+  implicit val bookingRequestValidator
+    : Validator[BookingRequest, BookingRequest] = { entity: BookingRequest =>
+    validateAmount _ apply entity
+  }
+
+  def validateDateFormat(
+    request: BookingOverviewRequest
+  ): ValidatedNel[String, LocalDate] = {
+    val parsedDate = DateUtils.toDateTime(request.performanceDate)
+    Either
+      .cond(parsedDate.isDefined, parsedDate.get, WRONG_DATE_FORMAT)
+      .toValidatedNel
+  }
+
+  implicit val dateValidator: Validator[BookingOverviewRequest, LocalDate] = {
+    entity =>
+      validateDateFormat _ apply entity
+  }
+
+}
